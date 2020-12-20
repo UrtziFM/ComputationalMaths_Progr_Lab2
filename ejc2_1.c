3313308fc6a9a3cbed0958ea2771724fd55a086b
UrtziFM <urtzi.arana13@gmail.com>
2020-12-20 18:23:02 +0100

    Starting the second activity

---
+++ ejc2_1.c
@@ -0,0 +1,15 @@
+/* 
+Este es el primer ejercicio del laboratorio 2: Implementaunprogramaque ofrezca un menú al usuario para realizar las siguientes operaciones con matrices 
+de números reales de tamaño 3x3:
+1.Suma de dos matrices,A y B
+2.Resta de dos matrices, A y B
+3.Multiplicación de dos matrices, A y B
+*/
+
+#include <stdio.h>
+#include <math.h> // I am using this library to use squirt & pow functions
+
+int main() {
+
+    return 0;
+}
